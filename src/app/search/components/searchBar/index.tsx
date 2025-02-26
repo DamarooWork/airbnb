8badd23d64a6766f9ad44cc67786dddfb4ce413f
Damaroo <levkichigin@yandex.ru>
2025-02-26 15:02:50 +0300

    daisyui add

--- src/app/search/components/searchBar/index.tsx
+++ src/app/search/components/searchBar/index.tsx
@@ -5,22 +5,27 @@ export default function SearchBar() {
   return (
     <section className="flex self-center rounded-full border p-2 mt-8 w-3/4">
       <button onClick={() => setIsSearchFocused(true)}>
+        <p className="font-bold">Where</p>
         {isSearchFocused ? (
-          <input type="text" placeholder="Search destinations" />
+          <input
+            className="text-slate-800 bg-transparent border-none outline-none"
+            type="text"
+            placeholder="Search destinations"
+          />
         ) : (
-          <p>Search destinations</p>
+          <p className="text-slate-600">Search destinations</p>
         )}
       </button>
-      <section>
+      <section className="dropdown dropdown-end px-4 border-r">
         <label htmlFor="">
-          <p>Dates</p>
-          <p>Select Ranged</p>
+          <p className="font-bold">Dates</p>
+          <p className="text-slate-600">Select Ranged</p>
         </label>
       </section>
       <section>
         <label htmlFor="">
-          <p>Who</p>
-          <p>Add Guests</p>
+          <p className="font-bold">Who</p>
+          <p className="text-slate-600">Add Guests</p>
         </label>
       </section>
     </section>
