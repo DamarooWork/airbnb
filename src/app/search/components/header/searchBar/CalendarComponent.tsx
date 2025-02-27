7f07c9d1441a180d939eada03fca158944ac211d
Damaroo <levkichigin@yandex.ru>
2025-02-27 17:59:06 +0300

    fix: delete usestate for deploy

--- src/app/search/components/header/searchBar/CalendarComponent.tsx
+++ src/app/search/components/header/searchBar/CalendarComponent.tsx
@@ -1,10 +1,13 @@
 import 'react-date-range/dist/styles.css' // main style file
 import 'react-date-range/dist/theme/default.css' // theme css file
 import { DateRangePicker, RangeKeyDict } from 'react-date-range'
-import { useState } from 'react'
 import { useSearchStore } from '@/store/SearchStore'
 
-export default function CalendarComponent({handleSelect}:{handleSelect: (startDate: Date, endDate: Date)=>void}) {
+export default function CalendarComponent({
+  handleSelect,
+}: {
+  handleSelect: (startDate: Date, endDate: Date) => void
+}) {
   const startDate = useSearchStore((state) => state.dates[0])
   const endDate = useSearchStore((state) => state.dates[1])
   const selectionRange = {
@@ -19,7 +22,6 @@ export default function CalendarComponent({handleSelect}:{handleSelect: (startDa
       })
       handleSelect(ranges.selection.startDate, ranges.selection.endDate)
     }
-   
   }
   return (
     <DateRangePicker
