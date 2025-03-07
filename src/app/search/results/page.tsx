762216cbf1de28ea89b1ff511b1fab95e8707f61
Damaroo <levkichigin@yandex.ru>
2025-03-07 21:05:29 +0300

    add: animation for like

--- src/app/search/results/page.tsx
+++ src/app/search/results/page.tsx
@@ -1,8 +1,14 @@
 import Link from 'next/link'
 import { IListing } from '../../api/search/route'
 import ResultsList from './components/resultsList'
+import { useRouter } from 'next/router';
 
 export default async function Results() {
+  const router = useRouter();
+
+  if (router.isFallback) {
+    <h1>Data is loading</h1>;
+  }
   const res = await fetch('http://localhost:3000/api/search')
   const data: IListing[] = await res.json()
   return (
