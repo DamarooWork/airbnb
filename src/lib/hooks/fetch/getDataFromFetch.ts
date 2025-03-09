7fd581c42639c0eac055cd7cc34b3d49f9e7301a
Damaroo <levkichigin@yandex.ru>
2025-03-09 21:24:49 +0300

    fix: deploy errors

--- src/lib/hooks/fetch/getDataFromFetch.ts
+++ src/lib/hooks/fetch/getDataFromFetch.ts
@@ -1,7 +1,8 @@
 'use server'
 
+import { BASE_URL } from '@/lib/utils/getUrl'
+
 export default async function getDataFromFetch() {
   const response = await fetch(`${BASE_URL}/api/search`)
   return await response.json()
 }
-
--- src/lib/utils/getUrl.ts
+++ src/lib/utils/getUrl.ts
@@ -1,4 +1,4 @@
-const BASE_URL =
+export const BASE_URL =
   process.env.NODE_ENV === "production"
     ? `https://${process.env.NEXT_PUBLIC_URL}`
     : "http://localhost:3000";
