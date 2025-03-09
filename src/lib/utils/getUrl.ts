fa09bd905bf9ee87f138acf424eaa5e6d065eadc
Damaroo <levkichigin@yandex.ru>
2025-03-09 21:25:31 +0300

    fix: deploy errors

--- src/lib/utils/getUrl.ts
+++ src/lib/utils/getUrl.ts
@@ -1,4 +1,4 @@
 export const BASE_URL =
-  process.env.NODE_ENV === "production"
-    ? `https://${process.env.NEXT_PUBLIC_URL}`
-    : "http://localhost:3000";
+  process.env.NODE_ENV === 'production'
+    ? `${process.env.NEXT_PUBLIC_URL}`
+    : 'http://localhost:3000'
