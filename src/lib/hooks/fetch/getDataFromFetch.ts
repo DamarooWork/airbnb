58e4d28c17714997df3bc992229ef560ff76769c
Damaroo <levkichigin@yandex.ru>
2025-03-09 21:22:58 +0300

    fix: deploy errors

--- src/lib/hooks/fetch/getDataFromFetch.ts
+++ src/lib/hooks/fetch/getDataFromFetch.ts
@@ -1,6 +1,7 @@
 'use server'
+
 export default async function getDataFromFetch() {
-  const response = await fetch('http://localhost:3000/api/search')
+  const response = await fetch(`${BASE_URL}/api/search`)
   return await response.json()
 }
 
