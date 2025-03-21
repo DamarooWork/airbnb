d6b1323523936178fdc9f76fe4f275e928a2271c
Damaroo <levkichigin@yandex.ru>
2025-03-22 02:47:55 +0300

    fix: deploy

--- src/app/search/results/page.tsx
+++ src/app/search/results/page.tsx
@@ -1,5 +1,4 @@
 'use client'
-import Link from 'next/link'
 import ListingList from '../../../ui/listing/List'
 import useGetListings from '@/hooks/fetch/useGetListings'
 import { useCallback, useEffect, useMemo, useState } from 'react'
