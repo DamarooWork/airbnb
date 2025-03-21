d17953ab18b0dc6f8562773863147af6dea3bb0b
Damaroo <levkichigin@yandex.ru>
2025-03-22 02:22:37 +0300

    add: delete action for listings

--- src/app/ui/header/headerLogo.tsx
+++ src/app/ui/header/headerLogo.tsx
@@ -4,29 +4,31 @@ import { useWindowSize } from '@uidotdev/usehooks'
 export default function HeaderLogo() {
   const size = useWindowSize()
   return (
-    <Link
-      aria-label="Airbnb homepage"
-      className="flex relative w-[50px] h-[50px] lg:w-[160px] lg:h-[50px] cursor-pointer"
-      href={'/search'}
-    >
-      {size.width && size.width < 1024 ? (
-        <Image
-          className="object-contain "
-          src={'/logos/airbnbLogoMini.png'}
-          fill
-          sizes="50px 50px"
-          alt="Airbnb mini logo"
-        />
-      ) : (
-        <Image
-          className="object-contain "
-          src={'/logos/logo.png'}
-          fill
-          sizes="160px 50px"
-          alt="Airbnb logo"
-          priority
-        />
-      )}
-    </Link>
+    <>
+      <Link
+        aria-label="Airbnb homepage"
+        className="flex relative w-[50px] h-[50px] lg:w-[160px] lg:h-[50px] cursor-pointer"
+        href={'/search'}
+      >
+        {size.width && size.width < 1024 ? (
+          <Image
+            className="object-contain "
+            src={'/logos/airbnbLogoMini.png'}
+            fill
+            sizes="50px 50px"
+            alt="Airbnb mini logo"
+          />
+        ) : (
+          <Image
+            className="object-contain "
+            src={'/logos/logo.png'}
+            fill
+            sizes="160px 50px"
+            alt="Airbnb logo"
+            priority
+          />
+        )}
+      </Link>
+    </>
   )
 }
